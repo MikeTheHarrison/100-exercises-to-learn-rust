894a688c3afd70f11a16fc9f988dcb498a4eea29
MLH <ninjapiehole@hotmail.com>
2025-07-31 16:26:32 -0700

    Lot's of bsic exercises done on lunch break. Good refresher.

--- exercises/03_ticket_v1/09_heap/src/lib.rs
+++ exercises/03_ticket_v1/09_heap/src/lib.rs
@@ -13,7 +13,11 @@ mod tests {
 
     #[test]
     fn string_size() {
-        assert_eq!(size_of::<String>(), todo!());
+        // 3 8-byte (64b) components.
+        // reference to data on heap
+        // length
+        // capacity
+        assert_eq!(size_of::<String>(), 24);
     }
 
     #[test]
@@ -23,6 +27,6 @@ mod tests {
         // but, in general, the memory layout of structs is a more complex topic.
         // If you're curious, check out the "Type layout" section of The Rust Reference
         // https://doc.rust-lang.org/reference/type-layout.html for more information.
-        assert_eq!(size_of::<Ticket>(), todo!());
+        assert_eq!(size_of::<Ticket>(), 3 * 24);
     }
 }
